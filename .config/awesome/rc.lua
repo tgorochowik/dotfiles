2e923e96294c2323adccdc9965cdc6c3610425b8
Tomasz Gorochowik <tgorochowik@gmail.com>
2017-02-10 23:04:32 +0100

    awesome: make urxvt transparent

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -484,6 +484,13 @@ awful.rules.rules = {
         }
       }, properties = { floating = true }},
 
+    -- Transparent windows
+    { rule_any = {
+        class = {
+          "URxvt"
+        },
+      }, properties = { opacity = 0.95 }},
+
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
