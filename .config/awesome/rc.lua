a31c590255ff1ebc4fdcf3cd53a6f315c6d858d5
Tomasz Gorochowik <tgorochowik@gmail.com>
2017-02-10 19:17:40 +0100

    awesome: remove systray

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -149,7 +149,6 @@ awful.screen.connect_for_each_screen(function(s)
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
-            wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
         },
