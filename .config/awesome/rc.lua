d32fe8871fe4e1007ac56a19fec48f3ac091621a
Tomasz Gorochowik <tgorochowik@gmail.com>
2017-02-10 19:06:31 +0100

    awesome: use my custom theme
    Hopefully it is fully compatible with 4.0

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -35,7 +35,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+beautiful.init("~/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
